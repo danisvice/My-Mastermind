3642f1ee9b72bbaa4284e21400f8d73e64163d7f
danny ortiz <ortiz_d@qwasar.io>
2023-01-04 21:04:42 +0000

    my_mastermind.c

--- my_mastermind.c
+++ my_mastermind.c
@@ -6,7 +6,7 @@
 int main()
 {
     int num_ok, pos_no_ok;
-    int i;
+    int i, j;
     int tentativo;
     int num_tentativi = 0;
 
@@ -37,11 +37,59 @@ int main()
             usata[i] = 0;
             usata_s[i] = 0;
         }
+
+    printf("Numero tentativo %d", num_tentativi+1);
+    printf("Will you find the secret code? Please enter a valid guess");
+    scanf("%d", &tentativo);
+
+    for(i = CIFRE-1; i >= 0; i--)
+    {
+        tentativo_utente[i] = tentativo % 10;
+        tentativo /= 10;
+    }
+
+    for(i = 0; i < CIFRE; i++)
+    {
+         if(tentativo_utente[i] == codice[i])
+         {
+             num_ok++;
+             usata[i] = 1;
+             usata_s[i] = 1;
+         }
+    }
+
+    for(i = 0; i < CIFRE; i++)
+    {
+        if(usata_s[i] == 0)
+        {
+            for(j = 0; j < CIFRE; j++)
+            {
+                if(tentativo_utente[i] == codice[j] && usata_s[j] == 0)
+                {
+                    pos_no_ok++;
+                    usata_s[j] = 1;
+                    break;
+                }
+            }
+        }
+
+        printf("Well placed pieces: %d\n", num_ok);
+        printf("Misplaced pieces: %d\n", pos_no_ok);
+
+        num_tentativi++;
+
+        if(num_ok == CIFRE)
+        {
+            num_tentativi = 10;
+        }
+    }
+
     }
     while(num_tentativi != 10);
 
-    printf("Will you find the secret code? Please enter a valid guess");
-    scanf("%d", &tentativo);
+    (num_tentativi == 10 && num_ok != CIFRE) 
+    ? printf("you lost!") 
+    : printf("Congratz! You did it!");
 
     return 0;
 }
