fc62f767f876e1f240f06cbe0d35e327d4aec364
danny ortiz <ortiz_d@qwasar.io>
2023-01-27 01:41:13 +0000

    my_mastermind.c updated

--- my_mastermind.c
+++ my_mastermind.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define MAX_ATTEMPTS 10
 #define CODE_LENGTH 4
@@ -61,18 +62,49 @@ int main(int argc, char **argv)
 
         for(int j = 0; j < bytes_read; j++)
         {
-            if(guess[j] == '\n')
-            {
-               guess[j] = '\0';
-               break;
-            }
+               if (strlen(guess) != CODE_LENGTH) 
+        {
+        // Print result
+        printf("Well placed pieces: %d\n", well_placed);
+        printf("Misplaced pieces: %d\n", misplaced);
+
         }        
 
         // Check for invalid input
-        if (strlen(guess) != CODE_LENGTH) {
-            printf("Wrong input!\n");
+        if (strlen(guess) != CODE_LENGTH) 
+        {
+           // Print result
+            printf("Well placed pieces: %d\n", well_placed);
+            printf("Misplaced pieces: %d\n", misplaced);
             continue;
         }
+        }
+
+        // Check for well-placed and misplaced pieces
+            for(int j = 0; j < bytes_read; j++)
+        {
+               if (strlen(guess) != CODE_LENGTH) 
+        {
+        // Print result
+        printf("Well placed pieces: %d\n", well_placed);
+        printf("Misplaced pieces: %d\n", misplaced);
+
+        }        
+
+        //Check for invalid input
+        // int fd = open("input_string", O_RDONLY);
+        // int bytes_read = 0;
+
+        // bytes_read = read(fd, guess, CODE_LENGTH);
+
+        // if (bytes_read != CODE_LENGTH) 
+        // {
+        // // Print result
+        //     printf("Well placed pieces: %d\n", well_placed);
+        //     printf("Misplaced pieces: %d\n", misplaced);
+        // continue;
+        // }
+
 
         // Check for well-placed and misplaced pieces
         for (int j = 0; j < CODE_LENGTH; j++) 
@@ -99,11 +131,10 @@ int main(int argc, char **argv)
             return 0;
         }
 
-        // Print result
+        //Print result
         printf("Well placed pieces: %d\n", well_placed);
         printf("Misplaced pieces: %d\n", misplaced);
-    }
 
-    printf("You lose!\n");
-    return 0;
-}
+    }
+    }
+};
