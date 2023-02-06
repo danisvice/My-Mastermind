911805615607266ce95b222ad797874ab9609e2d
danny ortiz <ortiz_d@qwasar.io>
2023-02-06 19:44:21 +0000

    my_mastermind.c complete

--- my_mastermind.c
+++ my_mastermind.c
@@ -14,16 +14,18 @@
 // #define LINE_LEN    (READ_LEN + 1)
 // change naming conv (user/code (buf))
 
+//*(buf)++; ! (*buf)++
 void print_char_buf(char* buf, int size)
 {
     printf("[ ");
     while (size--)
-        printf(size == 0 ? "\'%c\'" : "\'%c\', ", *buf++);//*(buf)++; ! (*buf)++
+    printf(size == 0 ? "\'%c\'" : "\'%c\', ", *buf++);
     printf(" ]\n");
 }
 
 // Generate random code if none specified
-void generate_code(char* code) {
+void generate_code(char* code) 
+{
     if (!code[0])// if (code[0] == 0)
     {
         for (int round = 0; round < CODE_LEN; round++) 
@@ -35,7 +37,8 @@ void generate_code(char* code) {
     print_char_buf(code, CODE_LEN); // debug! to be removed
 }
 
-// handles wrong inputs (too long or too short) // Check for invalid input
+// handles wrong inputs (too long or too short) 
+// Check for invalid input
 void check_user_input(bool* valid_input, char input)
 {
     if (valid_input == false || input != '\n') 
@@ -68,6 +71,7 @@ bool iswin(int well_placed)
     }
     return false;
 }
+
 // Check for well-placed and misplaced pieces
 void check_placing(char* guess, char* code, int* well_placed, int* misplaced)
 {
@@ -99,33 +103,6 @@ void check_placing(char* guess, char* code, int* well_placed, int* misplaced)
     }
 }
 
-// void check_placing(char* guess, char* code, int* well_placed, int* misplaced)
-// {
-//     for(int round = 0; round < CODE_LEN; round++)
-//     {
-//         for (int j = 0; j < CODE_LEN; j++) 
-//         {
-//             if (guess[j] == code[j]) 
-//             {
-//                 (*well_placed)+=1;
-//                 break;
-//             }
-//             else
-//             {
-//                 for (int k = 0; k < CODE_LEN; k++) 
-//                 {
-                    
-//                     if (guess[j] == code[k]) 
-//                     {
-//                         (*misplaced)+=1;
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
 
 int main(int argc, char **argv)
 {
@@ -156,9 +133,11 @@ int main(int argc, char **argv)
     }
     generate_code(code);
     // Print intro
+    // void greet_user(void);
     printf( "Will you find the secret code?\n" 
-            "Please enter a valid guess\n");// <-- void greet_user(void);
-    // For-Loop for each attempt
+            "Please enter a valid guess\n");
+
+    //  Conditional statement for each attempt
     while (round < rounds && !iswin(well_placed)) 
     {
         char guess[READ_LEN + 1] = {0};// '\0'<-- not good? + cREAD_LEN vs CODE..
@@ -198,63 +177,15 @@ int main(int argc, char **argv)
             guess[index] = input;// <-- should be '\n'
             index += 1;
         }
-        // buf['0', '\n', 0 0 0]
-        // while (input != '\n') {// || input != '\0'
-        //     printf("input: \'%c\'\n", input);
-        //     read(0, &input, 1);
-        // }
-        // input != '\n' --> input too long
-        // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'\n'
-        // ^^ we need to consume the rest of user input to get a fresh new user prompt for next iteration
-        
+
         check_user_input(&valid_input, input);
         if (valid_input == false)
             continue;
-        // Improvements: ^^ find a better way to catch '\n' than adding it to buf
-        // add compiler flags (|#define) for config CPU/MEM usage and debug
-        // code guess input len > CODE_LEN
-
-        // while ((read(0, &input, 1) > 0)) {
-        //     if (index < CODE_LEN) {
-        //         read(0, &input, 1);
-        //         guess[index] = input;
-        //         index += 1;
-        //     }
-        //     else {
-        //         input = '\n';
-        //     }
-        // }
-        print_char_buf(guess, sizeof(guess));//sizeof operator less efficient here!
-        // while (read(0, &input, 1) > 0 && input != '\n' && index < CODE_LEN - 1) {
-            // printf("\'%c\'\n", input);
-            // guess[index] = input;
-            // index++;
-        // }// read 4 no matter
-        // index = 0;
-        // while (index < CODE_LEN - 1) {
-        //     if (!isdigit(input) || ((guess[CODE_LEN - 1] == '\0' || guess[CODE_LEN - 1] == '\n'))) {
-        //         printf("invalid input!\n");
-        //         continue;
-        //     }
-        //     index += 1;
-        // }
-        // if(nbytes <= 0)
-        //     {
-        //         printf("\n");
-        //         break;
-        //     }
-        // guess[nbytes] = '\0';
-        // printf("user input %d\n", round+1);
-        //Check for invalid input
-        // if(strlen(guess) != CODE_LEN) // tata != valid
-        // {
-        //     continue;
-        // }
+        
+        //sizeof operator less efficient here!
+        print_char_buf(guess, sizeof(guess));
+        
         check_placing(guess, code, &well_placed, &misplaced);
-        // for(int j = 0; j < nbytes; j++)
-        // {
-        //     break;
-        // }// wtf
         if (iswin(well_placed) == true) 
         {
             return EXIT_SUCCESS;
