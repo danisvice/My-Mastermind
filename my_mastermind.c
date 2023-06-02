ff7ee1f91838e5f9489768832d7519bc39d89315
/x/./0x <20178529+danisvice@users.noreply.github.com>
2023-06-02 16:11:15 -0700

    updated: removed printf() of generated code, char_buf

--- my_mastermind.c
+++ my_mastermind.c
@@ -33,8 +33,8 @@ void generate_code(char* code)
             code[round] = rand() % 10 + '0';
         }
     }// mv to own function simplicity!!!
-    printf("Random code: "); // debug! to be removed
-    print_char_buf(code, CODE_LEN); // debug! to be removed
+    //printf("Random code: "); // debug! to be removed
+    //print_char_buf(code, CODE_LEN); // debug! to be removed
 }
 
 // handles wrong inputs (too long or too short) 
